c64dbeeb166ccbee7d9857bee6500a04bcc11ca9
cataususan <cataususan@gmail.com>
2024-01-08 18:57:11 +0200

    Adding map and button for frontend

---
+++ DATC ADMIN DASHBOARD/admin-dashboard/src/pages/StartingPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import MapComponent from "../components/StartingPageComponents/MapComponent";
+const StartingPage = () => {
+  return <MapComponent></MapComponent>;
+};
+
+export default StartingPage;
