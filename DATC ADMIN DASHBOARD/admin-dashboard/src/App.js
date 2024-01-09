bc0d7b9941423194f3e2d8ce724e25b4bed24b2b
cataususan <cataususan@gmail.com>
2024-01-09 18:41:19 +0200

    Restoring admin interface

--- DATC ADMIN DASHBOARD/admin-dashboard/src/App.js
+++ DATC ADMIN DASHBOARD/admin-dashboard/src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import StartingPage from "./pages/StartingPage";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
 
@@ -9,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/login" element={<StartingPage />} />
+        <Route exact path="/login" element={<Login />} />
         <Route exact path="/signup" element={<Signup />} />
         <Route
           exact
--- DATC ADMIN DASHBOARD/admin-dashboard/src/pages/StartingPage.jsx
+++ DATC ADMIN DASHBOARD/admin-dashboard/src/pages/StartingPage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import MapComponent from "../components/StartingPageComponents/MapComponent";
 import ApiMapComponent from "../components/StartingPageComponents/ApiMapComponent";
 const StartingPage = () => {
   return <ApiMapComponent></ApiMapComponent>;
